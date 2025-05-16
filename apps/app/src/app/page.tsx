bfbcdce289c9a1b7a58163f8b846c528c3eaff93
khalil-bchir <mohamedkhalil.bchir@polytechnicien.tn>
2025-05-16 11:31:34 +0100

    feat(initial commit): initila saas boilerplate setup

---
+++ apps/app/src/app/page.tsx
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <div>
+        <h1 className="text-2xl font-bold">Welcome to the App</h1>
+        <p>This is a protected route.</p>
+    </div>
+  );
+}
